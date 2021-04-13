9b8246d77834f0530a31efd409d3a34ddc46c352
YAYALUOYA <yayaluoya@sina.com>
2021-04-13 19:22:19 +0800

    ''

--- testConfig.js
+++ testConfig.js
@@ -1,7 +1,7 @@
 /** 配置数据 */
 module.exports = {
     /** 代理src目录，可以是绝对路径或者相对路径 */
-    src: 'F:/item/Laboratory/src/',
+    src: './test/src/',
     /** 代理bin目录，可以是绝对路径或者相对路径 */
-    bin: 'F:/item/Laboratory/bin/',
+    bin: './test/bin/',
 }
