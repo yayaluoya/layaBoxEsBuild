ab454188dab259e58d42fb8be2c9ec8d36b98a54
yayaluoya <yayaluoya@sina.com>
2021-07-14 20:47:29 +0800

    增加导入npm包的功能

--- test/src/Main.ts
+++ test/src/Main.ts
@@ -2,13 +2,19 @@ import _module from './module.txt';
 import './js.test.js';
 import './com/test';
 import 'src/ts';
+
+import axios from 'axios';
+const dayjs = require('dayjs');
+import dayjs1 = require('dayjs');
+
 import { Test, Modle__ } from './test';
 
 console.log(_module);
-
-console.log('测试', 301);
+console.log('测试', 306);
 
 let a: Test = new Test();
 let b: Modle__.M = new Modle__.M();
 
 console.log('d.ts文件内容', a, b);
+
+console.log('npm包测试', axios, dayjs);
